f088ca88a1f37444ffc582ef7c12cb28739e9e91
philip-rafael <hello@philiprafael.com>
2025-04-12 14:04:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ import os
 # --- Streamlit Page Setup ---
 st.set_page_config(page_title="Threatscape Global Map", layout="wide")
 st.title("🌍 Global Threatscape: Visualising Cyber Threat Activity")
-st.markdown("Daily threat intelligence on high-confidence malicious IP activity by country")
+st.markdown(
+    "Daily threat intelligence on high-confidence malicious IP activity by country"
+    )
 
 # --- Load cached data ---
 DATA_FILE = "cached_threat_data.json"
